6b1410620a50c7e21d192920fefc161df98eeb63
Ceribit <ceribit@gmail.com>
2018-10-31 23:39:31 -0500

    Added basic model without room

---
+++ Using-Libraries/app/src/main/java/com/ceribit/android/practicewithroom/Video.java
@@ -0,0 +1,43 @@
+package com.ceribit.android.practicewithroom;
+
+public class Video {
+
+    private String title;
+    private String description;
+    private String url;
+    private String creator;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+
+}
